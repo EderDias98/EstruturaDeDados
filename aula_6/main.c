a2d94ee8ad84abf2148a650a45d0bcf219fd5a36
ederxdias <ederxdias98@gmail.com>
2024-03-27 16:45:38 -0300

    aula_6

---
+++ aula_6/main.c
@@ -0,0 +1,34 @@
+#include "matriz.h"
+#include <stdio.h>
+
+Matriz* LerMatriz(){
+    int l,c,elem;
+    scanf("%d %d%*c",&l,&c);
+    Matriz* mat = inicializaMatriz(l,c);
+    
+    for( int i=0; i<l;i++){
+        for(int j=0; j<c;j++){
+            scaf("%d%*c",&elem);
+            modificaElemento(mat,i,j,elem);
+        }
+    }
+    return mat;
+}
+
+
+int main(int argc, int *argv[]){
+
+
+    Matriz* mat1 = LerMatriz();
+    Matriz* mat2 = LerMatriz();
+
+    Matriz* mat1t = transposta(mat1);
+    imprimeMatriz(mat1t);
+    
+    Matriz* mat_m = multiplicacao(mat1,mat2);
+    if(mat_m){
+        imprimeMatriz(mat_m);
+    }
+
+    return 0;
+}
